688221b173873a8b46b1c9b16e4c3c25b6c2bbfc
Eduardo Saraiva <saraiva.eduardo@hotmail.com>
2023-09-15 02:26:24 -0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import cors from "cors"
+import express, { request, response, text } from "express"
+
+import { convert } from "./convert.js"
+import { download } from "./download.js"
+import { transcribe } from "./transcribe.js"
+import { summarize } from "./summarize.js"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.get("/summary/:id", async (request, response) => {
+  //try{"parei aqui !!!!"}
+  await download(request.params.id)
+  const audioConverted = await convert
+  const result = await transcribe(audioConverted)
+
+  return response.json({ result })
+})
+
+app.post("./summary", async (request, response) => {
+  const result = await summarize(request.body.text)
+  return response.json({ result })
+})
+
+app.listen(3333, () => console.log("Server is run on port 3333"))
---
+++ server/transcribe.js
@@ -0,0 +1,12 @@
+//import { Pipeline } from "@xenova/transformers"
+import { transcriptionExample } from "./utils/transcription.js"
+
+export async function transcribe() {
+  return transcriptionExample
+
+  //  try {
+
+  //  } catch (error)
+  //      throw new Error(erro)
+  //}
+}
